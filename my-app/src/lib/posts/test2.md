b1ebb3dfd6f08d3f9c6c3c3111bce47f3564c237
Anne <144004647+Annevd@users.noreply.github.com>
2024-09-30 16:23:18 +0200

    #4 md rendering update
    De files worden nu correct ingeladen op de homepage. Ook heb ik een slug route aangemaakt voor de blog posts pagina.
    
    De volgende stap is om de markdown als HTML te renderen in de blog posts

---
+++ my-app/src/lib/posts/test2.md
@@ -0,0 +1,8 @@
+---
+title: 'Does it work??'
+slug: 'it-works'
+---
+
+# Hello to the World
+
+This is my first blog post!
--- my-app/src/routes/blog/+page.svelte
+++ my-app/src/routes/blog/+page.svelte
@@ -1,12 +1,12 @@
 <script>
     // This variable will receive the posts from the load function in +page.server.js
-    export let posts = [];
-    console.log('Posts received in Svelte:', posts);
+    export let data = [];
+    console.log('Posts received in Svelte:', data);
 </script>
 
 <h1>We love web Blog Posts</h1>
-{#if posts.length > 0}
-    {#each posts as post}
+{#if data.posts && data.posts.length > 0}
+    {#each data.posts as post}
         <p>
             <a href="/blog/{post.slug}">{post.title}</a>
         </p>
