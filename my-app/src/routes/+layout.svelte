75277ae2634a8b34b940495613a7652b4da67e80
Anne <144004647+Annevd@users.noreply.github.com>
2024-09-30 10:20:54 +0200

    added about page

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <nav>
     <ul>
         <li><a href="/">Home</a></li>
+        <li><a href=/about>About me</a></li>
         <li><a href="/blog">We love webs</a></li>
         <li><a href="/gallery">Art gallery</a></li>
     </ul>
